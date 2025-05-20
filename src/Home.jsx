f463c3256354074b6c0ab125a6803d02edad206b
guigarmon <154552767+guigarmon@users.noreply.github.com>
2025-05-20 08:07:57 +0200

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main style={{ backgroundColor: 'black', color: 'white', minHeight: '100vh', textAlign: 'center', paddingTop: '5rem' }}>
+      <h1>Bienvenue dans B*WITCH*</h1>
+      <button style={{ padding: '1rem 2rem', marginTop: '2rem' }}>Je suis prête</button>
+    </main>
+  )
+}
